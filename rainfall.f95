3e00b937a5b785ef9dc48c49ad17a23bb72511f1
jlihnarop <jlihnarop@uth.gr>
2023-11-20 09:01:26 +0200

    c4 - Read and display all items (up to 50) from data.txt

--- rainfall.f95
+++ rainfall.f95
@@ -1,17 +1,20 @@
 program rainfall
 implicit none
-integer::i
-character(20)::town(5)
-integer::rain(5)
+integer,parameter::Nmax=50
+integer::i,k,st,N
+character(20)::town(Nmax)
+integer::rain(Nmax)
 
   open(10,file='data.txt')
   read(10,*)
-  
-  do i=1,5
-    read(10,*) town(i),rain(i)
+  k=1
+  do 
+    read(10,*,iostat=st) town(k),rain(k)
+    if (st/=0) exit
+    k=k+1
   end do
-
-  do i=1,5
+  N=k-1
+  do i=1,N
     print*,town(i),rain(i)
   end do
 end program
