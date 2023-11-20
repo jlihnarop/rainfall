93e145385ebb69d47f6ba37035fe6002ebf6eb6f
jlihnarop <jlihnarop@uth.gr>
2023-11-20 08:54:08 +0200

    c2 - Read and display 5 items from data.txt

--- rainfall.f95
+++ rainfall.f95
@@ -1,4 +1,17 @@
 program rainfall
 implicit none
+integer::i
+character(20)::town(5)
+integer::rain(5)
 
+  open(10,file='data.txt')
+  read(10,*)
+  
+  do i=1,5
+    read(10,*) town(i),rain(i)
+  end do
+
+  do i=1,5
+    print*,town(i),rain(i)
+  end do
 end program
